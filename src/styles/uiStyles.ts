e5ce9549ab859ab6a9ce706cd279de442afadb02
17u5h <102058870+17u5h@users.noreply.github.com>
2023-07-29 00:05:11 +0700

    transitions corrected

--- src/styles/uiStyles.ts
+++ src/styles/uiStyles.ts
@@ -8,7 +8,7 @@ export const UiButton = styled.button`
   height: 5vh;
   padding: 0 1vw;
   color: #fff;
-  background-color: #565EEF;
+  background-color: ${vars.$colorThemeSecondary}
   border: none;
   border-radius: 10px;
   user-select: none;
@@ -16,15 +16,15 @@ export const UiButton = styled.button`
   transition: background-color 0.3s ease-in;
 
   &:hover {
-    background-color: #313bec;
+    background-color: ${vars.$colorThemeHover};
   }
 
   &:active {
-    background-color: #1520d3;
+    background-color: ${vars.$colorThemeActive};
   }
 
   &:disabled {
-    background-color: #a6a6a6;
+    background-color: ${vars.$colorThemeDisabled};
     cursor: not-allowed;
   }
 
